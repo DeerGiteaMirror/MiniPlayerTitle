60b28fe40a56216724a516ff5c56867a824194b9
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-02-04 14:56:43 +0800

    update motd

--- src/main/java/cn/lunadeer/miniplayertitle/MiniPlayerTitle.java
+++ src/main/java/cn/lunadeer/miniplayertitle/MiniPlayerTitle.java
@@ -23,17 +23,17 @@ public final class MiniPlayerTitle extends JavaPlugin {
         Objects.requireNonNull(Bukkit.getPluginCommand("MiniPlayerTitle")).setExecutor(new Commands());
         Objects.requireNonNull(Bukkit.getPluginCommand("MiniPlayerTitle")).setTabCompleter(new Commands());
 
-        String logo = "称号插件已加载 版本: " + getPluginMeta().getVersion() + "\n";
+        XLogger.info("称号插件已加载");
+        XLogger.info("版本: " + getPluginMeta().getVersion());
         // http://patorjk.com/software/taag/#p=display&f=Big&t=MiniPlayerTitle
-        logo += "  __  __ _       _ _____  _                    _______ _ _   _\n" +
-                " |  \\/  (_)     (_)  __ \\| |                  |__   __(_) | | |\n" +
-                " | \\  / |_ _ __  _| |__) | | __ _ _   _  ___ _ __| |   _| |_| | ___\n" +
-                " | |\\/| | | '_ \\| |  ___/| |/ _` | | | |/ _ \\ '__| |  | | __| |/ _ \\\n" +
-                " | |  | | | | | | | |    | | (_| | |_| |  __/ |  | |  | | |_| |  __/\n" +
-                " |_|  |_|_|_| |_|_|_|    |_|\\__,_|\\__, |\\___|_|  |_|  |_|\\__|_|\\___|\n" +
-                "                                   __/ |\n" +
-                "                                  |___/";
-        XLogger.info(logo);
+        XLogger.info("  __  __ _       _ _____  _                    _______ _ _   _\n");
+        XLogger.info(" |  \\/  (_)     (_)  __ \\| |                  |__   __(_) | | |\n");
+        XLogger.info(" | \\  / |_ _ __  _| |__) | | __ _ _   _  ___ _ __| |   _| |_| | ___\n");
+        XLogger.info(" | |\\/| | | '_ \\| |  ___/| |/ _` | | | |/ _ \\ '__| |  | | __| |/ _ \\\n");
+        XLogger.info(" | |  | | | | | | | |    | | (_| | |_| |  __/ |  | |  | | |_| |  __/\n");
+        XLogger.info(" |_|  |_|_|_| |_|_|_|    |_|\\__,_|\\__, |\\___|_|  |_|  |_|\\__|_|\\___|\n");
+        XLogger.info("                                   __/ |\n");
+        XLogger.info("                                  |___/");
     }
 
     @Override
