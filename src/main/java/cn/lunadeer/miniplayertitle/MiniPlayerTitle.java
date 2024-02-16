6ac28cc04cc45bab4f175dc2454916eac240ef57
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-02-16 12:56:08 +0800

    修复了控制台motd错误

--- src/main/java/cn/lunadeer/miniplayertitle/MiniPlayerTitle.java
+++ src/main/java/cn/lunadeer/miniplayertitle/MiniPlayerTitle.java
@@ -26,13 +26,13 @@ public final class MiniPlayerTitle extends JavaPlugin {
         XLogger.info("称号插件已加载");
         XLogger.info("版本: " + getPluginMeta().getVersion());
         // http://patorjk.com/software/taag/#p=display&f=Big&t=MiniPlayerTitle
-        XLogger.info("  __  __ _       _ _____  _                    _______ _ _   _\n");
-        XLogger.info(" |  \\/  (_)     (_)  __ \\| |                  |__   __(_) | | |\n");
-        XLogger.info(" | \\  / |_ _ __  _| |__) | | __ _ _   _  ___ _ __| |   _| |_| | ___\n");
-        XLogger.info(" | |\\/| | | '_ \\| |  ___/| |/ _` | | | |/ _ \\ '__| |  | | __| |/ _ \\\n");
-        XLogger.info(" | |  | | | | | | | |    | | (_| | |_| |  __/ |  | |  | | |_| |  __/\n");
-        XLogger.info(" |_|  |_|_|_| |_|_|_|    |_|\\__,_|\\__, |\\___|_|  |_|  |_|\\__|_|\\___|\n");
-        XLogger.info("                                   __/ |\n");
+        XLogger.info("  __  __ _       _ _____  _                    _______ _ _   _");
+        XLogger.info(" |  \\/  (_)     (_)  __ \\| |                  |__   __(_) | | |");
+        XLogger.info(" | \\  / |_ _ __  _| |__) | | __ _ _   _  ___ _ __| |   _| |_| | ___");
+        XLogger.info(" | |\\/| | | '_ \\| |  ___/| |/ _` | | | |/ _ \\ '__| |  | | __| |/ _ \\");
+        XLogger.info(" | |  | | | | | | | |    | | (_| | |_| |  __/ |  | |  | | |_| |  __/");
+        XLogger.info(" |_|  |_|_|_| |_|_|_|    |_|\\__,_|\\__, |\\___|_|  |_|  |_|\\__|_|\\___|");
+        XLogger.info("                                   __/ |");
         XLogger.info("                                  |___/");
     }
 
